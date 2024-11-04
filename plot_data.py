a8580350cd32fa7d59d8719db1e05dbbcc0f6394
facristiano <cristianof.azevedo@gmail.com>
2024-11-04 06:14:18 -0300

    Atualização

--- plot_data.py
+++ plot_data.py
@@ -1,108 +1,98 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+from datetime import datetime
 import io
 from PIL import Image
-from load_data import load_config, load_raw_data, load_xy_data, load_position_data, load_distance_data, load_data
 
-
-#load the cofiguration file
-config = load_config('config.yml')
-
-#load the raw data (downlink or uplink) from the .pkl file
-raw_data = load_raw_data ('08_08_2024-02_18_27.pkl', config['plot']['type_link'])
-
-#load position data
-uex_data, uey_data, bsx_data, bsy_data,uex_off_data, uey_off_data,bs_index_data = load_position_data(config['plot']['n_bs'], config['plot']['n_s'], raw_data)
-
-if config['plot']['x_var'] == 'distance' or config['plot']['y_var']=='distance':
-        if config['plot']['x_var']=='distance':
-              x_data = load_distance_data(config['plot']['n_bs'], config['plot']['n_s'], raw_data)
-              y_data = load_data(config['plot']['y_var'],config['plot']['n_bs'],config['plot']['n_s'], raw_data)
-        else:
-              y_data = load_distance_data(config['plot']['n_bs'], config['plot']['n_s'], raw_data)
-              x_data = load_data(config['plot']['x_var'],config['plot']['n_bs'],config['plot']['n_s'], raw_data)
-
-else:
-      x_data, y_data = load_xy_data(config['plot']['x_var'],config['plot']['y_var'],config['plot']['n_bs'],config['plot']['n_s'], raw_data)
 
 #customize the graph
-plt.figure(figsize=tuple(config['plot']['figsize']), dpi=config['plot']['resolution'])
-plt.title(config['plot']['title'])
-plt.xlabel(config['plot']['xlabel'])
-plt.ylabel(config['plot']['ylabel'])
-match config['plot']['auto_scale']:
-    case 'True':
-        plt.autoscale()
-    case 'False':
-        plt.xlim((config['plot']['xlim']))
-        plt.ylim(config['plot']['ylim'])
-plt.grid(config['plot']['grid'])
+def customize_graph (title=None, xlabel=None, ylabel=None, auto_scale=True, xlim=None, ylim=None, figsize=None,resolution=None, grid=None):
+        plt.figure(figsize=figsize, dpi=resolution)
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        match auto_scale:
+            case 'True':
+                plt.autoscale()
+            case 'False':
+                plt.xlim(xlim)
+                plt.ylim(ylim)
+        plt.grid(grid)
+        
+        return ()
+
+# save de graph
+def save_graph(complete=None, save_as=None, time=None):
+
+    output_dir = os.path.join("draw_output", time)
+    os.makedirs(output_dir, exist_ok=True)
+    save_path = os.path.join(output_dir, save_as)
+
+    if complete == False:
+        plt.show()
+        print('2) Your graph is ready!')
+        s = input("3) Do you want to save(y/n)?:")
+        match s:
+            case "y":
+                plt.savefig(save_path)
+            case "n":
+                print("4) Make the desired changes to config.yml!")
+            case _:
+                print("Value not accepted!")
+    else:
+        plt.savefig(save_path)
+        plt.close()
+
+    return ()
+
+# draw the graph               
+def draw_graph (x_var=None, y_var=None, n_bs=None, n_s=None, figsize=None, resolution=None, title=None, xlabel=None, ylabel=None, auto_scale=True, xlim=None, ylim=None, grid=None, raw_data=None):
+        if x_var == 'distance' or y_var == 'distance':
+              if x_var == 'distance':
+                     x_data = load_distance_data(n_bs, n_s, raw_data)
+                     y_data = load_data(y_var,n_bs,n_s, raw_data)
+              else:
+                     y_data = load_distance_data(n_bs,n_s,raw_data)
+                     x_data = load_data(x_var,n_bs,n_s,raw_data)
+
+        else:
+           x_data, y_data = load_xy_data(x_var,y_var,n_bs,n_s, raw_data)
+
+        #customize the graph
+        customize_graph (title, xlabel, ylabel, auto_scale=True, xlim=None, ylim=None)
+
+        #draw the graph
+        match graph_type:
+            case 'scatter':
+                fig =  sns.scatterplot(x=x_data, y=y_data,size=1)
+            case 'line':
+                fig = sns.lineplot(x=x_data, y=y_data)
+            case 'boxplot':
+                fig = sns.boxplot(x=x_data, y=y_data)
+            case 'bar':
+                fig = sns.barplot(x=x_data, y=y_data, ci=None)
+            case 'histplot':
+                fig = sns.histplot(x_data)
+            case _:
+                print("Check the 'graph_type' parameter!")
+        save_graph(False, save_as)
+        
+        return ()
 
 
-#draw the graph
-match config['plot']['graph_type']:
-    case 'scatter':
-         fig =  sns.scatterplot(x=x_data, y=y_data, color='purple', size=y_data, sizes=(1,1), legend=False)
-    case 'line':
-         fig = sns.lineplot(x=x_data, y=y_data)
-    case 'boxplot':
-         fig = sns.boxplot(x=x_data, y=y_data)
-    case 'bar':
-         fig = sns.barplot(x=x_data, y=y_data, ci=None)
-    case 'histplot':
-         fig = sns.histplot(x_data)
-    case _:
-         print("Check the 'graph_type' parameter!")
+def draw_map (uex_data, uey_data, bsx_data, bsy_data,uex_off_data, uey_off_data, bs_index_data, title, xlabel, ylabel, auto_scale=True, xlim=None, ylim=None,figsize=None,resolution=None, save_as=None, complete=False, grid=None, time=None ):
+                customize_graph(title, xlabel, ylabel, auto_scale, xlim, ylim, figsize, resolution, grid)
+                test = len(uex_off_data)
+                if test != 0:
+                    fig = sns.scatterplot(x=uex_off_data, y=uey_off_data, marker='o', size=10, color='red', legend=False)
+                else:
+                     {}
+                fig = sns.scatterplot(x=bsx_data, y=bsy_data, hue=bs_index_data, marker='^', size=bsx_data, sizes=(200,200),legend=False)
+                fig = sns.scatterplot(x=uex_data, y=uey_data,hue=bs_index_data, marker='o',size=uex_data,sizes=(10,10),legend=False)
+                save_graph(complete,save_as, time)
+                
+                return ()
 
-# save the graph
-buf = io.BytesIO()
-plt.savefig(buf, format='png')
-buf.seek(0)
-plt.close()
-imagem1 = Image.open(buf)
-imagem1.show()
-print('2) Your graph is ready!')
-s = input("3) Do you want to save your graph(y/n)?:")
-match s:
-    case "y":
-        imagem1.save(config['plot']['save_as'])
-        print("4) Graphic saved as " + str(config['plot']['save_as']) + "!")
-    case _:
-        print ("4) Make the desired changes to config.yml!")
-
-#customize the map
-plt.figure(figsize=tuple(config['plot']['figsize_map']), dpi=config['plot']['resolution_map'])
-plt.title(config['plot']['title_map'])
-plt.xlabel(config['plot']['xlabel_map'])
-plt.ylabel(config['plot']['ylabel_map'])
-plt.xlim(config['plot']['xlim_map'])
-plt.ylim(config['plot']['ylim_map'])
-
-#draw the map
-test = len(uex_off_data)
-if  test != 0:
-    fig = sns.scatterplot(x=uex_off_data, y=uey_off_data, marker='o',size=10, color='red', legend=False)
-else:
-    {}
-fig = sns.scatterplot(x=uex_data, y=uey_data,hue=bs_index_data, marker='o', palette='colorblind',size=10 ,legend=False)
-fig = sns.scatterplot(x=bsx_data, y=bsy_data, hue=bs_index_data, marker='^',size=bs_index_data,sizes=(200,200), palette="colorblind",legend=False)
-
-#save the map
-buf = io.BytesIO()
-plt.savefig(buf, format='png')
-buf.seek(0)
-plt.close()
-imagem2 = Image.open(buf)
-imagem2.show()
-print('5) Your map is ready!')
-
-s = input("6) Do you want to save your map(y/n)?:")
-
-match s:
-    case "y":
-        imagem2.save(config['plot']['save_map_as'])
-        print('7) Map saved as ' + str(config['plot']['save_map_as']) + '!')
-    case _:
-        print ("7) Make the desired changes to config.yml!")
 
 
